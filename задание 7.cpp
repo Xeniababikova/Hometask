d6e445081e317c66e680a0b3bb7c4d3ceb1671a6
Xeniababikova <63497984+Xeniababikova@users.noreply.github.com>
2020-05-19 20:37:48 +0300

    Add files via upload

---
+++ задание 7.cpp
@@ -0,0 +1,17 @@
+﻿// задание 7 (1).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "MIREA.h"
+using namespace std;
+int main()
+{
+    //Создать МИРЭА
+    MIREA mirea;
+    //Временные объекты типа "студент"
+    Student temp_student("Stud1", "stud1@mirea.ru", "1110-1101", 4, 1);
+    mirea.addStudent(temp_student);
+    Student temp_student2("Stud2", "stud2@mirea.ru", "1110-1101", 4, 2);
+    mirea.addStudent(temp_student2);
+    mirea.printStudents();
+}
